ba94d862d6e1ec44ca4df38a99bb14c4da7e0897
Jake Clause <99262232+JakeClause@users.noreply.github.com>
2022-02-17 07:53:21 -0500

    Update Main.java

--- randomHouses/src/Main.java
+++ randomHouses/src/Main.java
@@ -20,16 +20,15 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) throws IOException {
         PrintStream file = new PrintStream(new File("Houses.txt")); // creates a file with a path stream
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 5; i++) { // Replace the number after "i < " for many addresses you want
             Random rnd = new Random();
             int street1 = (rnd.nextInt(10));
             int house = (rnd.nextInt(10));
             char street2 = (char) ('a' + rnd.nextInt(26));
-            String address = (i+1) +".) (Street 1 (x): " + (street1+1) + ", Street 2 (y): " + street2 + ", House: " + (house+1)
-                    + ")";
+            String address = (i+1) +".) (Street 1 (x): " + (street1+1) + ", Street 2 (y): " + street2 + ", House: " + (house+1) + ")";
             //String ezAddress = (street1+1) + "," + street2 + "," + (house+1) + ")";
             System.setOut(file); // Selects file as output directory for everything below
-            System.out.println(address); // Prints the address to the file. Replace "address" with ezAdress" when needed
+            System.out.println(address); // Prints the address to the file. Replace "address" with "ezAdress" when needed
         }
     }
 }
