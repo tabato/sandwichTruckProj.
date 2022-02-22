1e5cb84a15449d1225391e7a7af7a73a2ef9fd7d
Jake Clause <99262232+JakeClause@users.noreply.github.com>
2022-02-22 10:33:51 -0500

    Made house numbers go by 10

--- randomHouses/src/Main.java
+++ randomHouses/src/Main.java
@@ -6,41 +6,33 @@ import java.util.Random;
     Jake Clause
 
     Description:
-    This creates 10 randomly generated address (coordinates) for the sandwich truck and puts them into a file called
+    This creates 100 randomly generated address (coordinates) for the sandwich truck and puts them into a file called
     "Houses.txt".
 
     Variables:
-    int street1 = x values (1-10)
+    int street1 = x values (0-10)
     char street2 = y values (a-z)
-    int house = Number value (1-10)
+    int house = Number value (0-10)
+    int block = Number value (0-8) + 1
     String address = Randomly generated address neatly formatted
     String ezAddress = Same values as address but only contains values for easy manipulation
-    String nHouse = Same value as house but when house < 10 a zero is added to the front of the house number
+    String nHouse = Merges the house and block values together to create a new house value
      */
 
 public class Main {
     public static void main(String[] args) throws IOException {
         PrintStream file = new PrintStream(new File("Houses.txt")); // creates a file with a path stream
-        for (int i = 0; i < 50; i++) { // Replace the number after "i < " for many addresses you want
+        for (int i = 0; i < 100; i++) { // Replace the number after "i < " for many addresses you want
             Random rnd = new Random();
             int street1 = (rnd.nextInt(10));
-            int house = (rnd.nextInt(99));
-            int block = (rnd.nextInt(9));
+            int house = (rnd.nextInt(9));
+            int block = (rnd.nextInt(8))+1;
             char street2 = (char) ('a' + rnd.nextInt(26));
-            if(house < 10){
-                String nHouse = "0" + (house); // adds a 0 to the front of the house number
-                //String address = (i+1) +".) (Street 1 (x): " + (street1+1) + ", Street 2 (y): " + street2 + ", House: " + (block+1)+nHouse + ")";
-                String ezAddress = "(" + (street1+1) + ", " + street2 + ", " +(block+1)+nHouse + ")";
-                System.setOut(file); // Selects file as output directory for everything below
-                System.out.println(ezAddress); // Prints the address to the file. Replace "address" with "ezAdress" when needed
-            }
-            else{
-                //String address = (i+1) +".) (Street 1 (x): " + (street1+1) + ", Street 2 (y): " + street2 + ", House: " + (block+1)+(house+1) + ")";
-                String ezAddress = "(" + (street1+1) + ", " + street2 + ", " +(block+1)+(house+1) + ")";
-                System.setOut(file); // Selects file as output directory for everything below
-                System.out.println(ezAddress); // Prints the address to the file. Replace "address" with "ezAdress" when needed
-            }
-
+            String nHouse = String.valueOf(block)+String.valueOf(house+1)+"0"; // adds a 0 to the front of the house number
+            //String address = (i+1) +".) (Street 1 (x): " + (street1+1) + ", Street 2 (y): " + street2 + ", House: " + (block+1)+nHouse + ")";
+            String ezAddress = "(" + (street1 + 1) + ", " + street2 + ", " + nHouse + ")";
+            System.setOut(file); // Selects file as output directory for everything below
+            System.out.println(ezAddress); // Prints the address to the file. Replace "address" with "ezAdress" when needed
         }
     }
 }
