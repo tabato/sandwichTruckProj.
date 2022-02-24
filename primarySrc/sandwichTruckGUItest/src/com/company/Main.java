3dd4b2a3b22e7713f0abb80993c67b66c0d73f03
Thomas Abato <abatot@moravian.edu>
2022-02-23 22:20:10 -0500

    Final Touch Ups for Sprint One

---
+++ primarySrc/sandwichTruckGUItest/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        mainFrame mainframe = new mainFrame();
+
+    }
+}
