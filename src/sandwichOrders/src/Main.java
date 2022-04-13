71e438ff1f7cc860ac696eb874490ed606c0953e
Jake Clause <clausej@moravian.edu>
2022-04-13 14:52:05 -0400

    Fixed null pointer error in sandwichOrders

--- src/sandwichOrders/src/Main.java
+++ src/sandwichOrders/src/Main.java
@@ -1,22 +1,37 @@
 package sandwichOrders2.src;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
+import java.text.DecimalFormat;
 
 import static java.lang.Thread.sleep;
+/*
+Author: Jake Clause
 
+
+This program makes randomly generated sandwich orders. While it is all randomly generated it is set up to allow customers
+pick whatever they want on the sandwich, and they can even have extra toppings or sauce on them. The final result is the
+sandwich type listed with the ingredients and total cost.
+
+
+ */
 public class Main {
+
+	private static final DecimalFormat df = new DecimalFormat("0.00");
+
 	public static void main(String[] args) throws FileNotFoundException {
+		float cashregister = 0;
+
 		PrintStream file = new PrintStream(new File("SandwichOrders.txt"));
 		// Print to file
 		System.setOut(file); // Selects file as output directory for everything below
 		for (int i = 0; i < 100; i++) {
-			new makeSandwiches();
-			try {
-				sleep(100);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+			makeSandwiches sandwich = new makeSandwiches();
+
+
+			cashregister += sandwich.cost();
 		}
+		System.out.println("Total: $"+df.format(cashregister));
 	}
 }
--- src/sandwichOrders/src/makeSandwiches.java
+++ src/sandwichOrders/src/makeSandwiches.java
@@ -1,5 +1,6 @@
 package sandwichOrders2.src;
 
+
 import java.text.DecimalFormat;
 import java.util.HashMap;
 import java.util.Random;
@@ -8,48 +9,30 @@ import static java.lang.Thread.sleep;
 
 public class makeSandwiches {
 
+    /*
+
+    This part of the program is what makes the sandwiches. It gathers the requested ingredients from where need and
+    creates the sandwiches.
+
+     */
 
 
     // This is used to format the cost into a $00.00 pattern
     private static final DecimalFormat df = new DecimalFormat("0.00");
     private static final Random rnd = new Random();
     private static final HashMap<Integer, Sandwich> sandwichHashMap = new HashMap<>();
-    private static final HashMap<Integer, Sandwich> toppingsHashMap = new HashMap<>();
+    //private static final HashMap<Integer, Sandwich> toppingsHashMap = new HashMap<>();
     private static Sandwich sandwichType;
+    public static float costOfsandwich = 0.00F;
 
     makeSandwiches() {
         initializeSandwichHashMap();
+        //initializeToppingsHashMap();
+        setSandwichType();
         randomSandwiches();
     }
 
-    public static Sandwich setSandwich() {
-        int sTypeInt = rnd.nextInt(3)+1;
-        sandwichType = sandwichHashMap.get(sTypeInt);
-        try {
-            sleep(200);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        if (sTypeInt == 1 || sTypeInt == 3){
-            int breadNum = rnd.nextInt(3)+1;
-            if (breadNum == 1){
-                sandwichType = new WhiteBread(sandwichType);
-            }if (breadNum == 2){
-                sandwichType = new WheatBread(sandwichType);
-            }if (breadNum == 3){
-                sandwichType = new RyeBread(sandwichType);
-            }
-        }else{
-            sandwichType = new Bun(sandwichType);
-        }
 
-        return sandwichType;
-    }
-
-    private Sandwich getSandwich() {
-        sandwichType = setSandwich();
-        return sandwichType;
-    }
 
     public void initializeSandwichHashMap(){
         sandwichHashMap.put(1, new Ham());
@@ -57,13 +40,8 @@ public class makeSandwiches {
         sandwichHashMap.put(3, new Turkey());
     }
 
+    /*
     public void initializeToppingsHashMap(){
-        sandwichType = getSandwich();
-        try {
-            sleep(200);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         toppingsHashMap.put(1, sandwichType =  new Ketchup(sandwichType));
         toppingsHashMap.put(2, sandwichType =  new Cheese(sandwichType));
         toppingsHashMap.put(3, sandwichType =  new Lettuce(sandwichType));
@@ -71,12 +49,80 @@ public class makeSandwiches {
         toppingsHashMap.put(5, sandwichType =  new Mustard(sandwichType));
         toppingsHashMap.put(6, sandwichType =  new Tomato(sandwichType));
     }
+     */
 
     public void randomSandwiches(){
-        initializeToppingsHashMap();
-        sandwichType = toppingsHashMap.get(rnd.nextInt(toppingsHashMap.size()));
+        sandwichType = getSandwichType();
+        getBreadTypes();
+        getRandomToppings();
+        /*
+        getBreadTypes();
+        getRandomToppings();
+         */
         System.out.println(sandwichType.getDescription() + ", $" +df.format(sandwichType.cost()));
     }
 
+    private Sandwich getSandwichType() {
+        return sandwichType;
+    }
+
+    public void getBreadTypes(){
+        if(sandwichType == sandwichHashMap.get(2)){
+            sandwichType = new Bun(sandwichType);
+
+        }else{
+            int breadType = rnd.nextInt(3);
+            if((breadType+1) == 1){
+                sandwichType = new WhiteBread(sandwichType);
+            }if((breadType+1) == 2){
+                sandwichType = new WheatBread(sandwichType);
+            }if((breadType+1) == 3){
+                sandwichType = new RyeBread(sandwichType);
+            }
+        }
+    }
+
+    public void getRandomToppings(){
+        boolean wantsToppings = rnd.nextBoolean();
+        while(wantsToppings){
+            boolean wantSauce = rnd.nextBoolean();
+            if (wantSauce){
+                boolean wantsKetchup = rnd.nextBoolean();
+                if (wantsKetchup){
+                    sandwichType = new Ketchup(sandwichType);
+                }boolean wantsMustard = rnd.nextBoolean();
+                if (wantsMustard){
+                    sandwichType = new Mustard(sandwichType);
+                }boolean wantsMayonnaise = rnd.nextBoolean();
+                if (wantsMayonnaise){
+                    sandwichType = new Mayonnaise(sandwichType);
+                }
+            }
+            boolean wantsCheese = rnd.nextBoolean();
+            if (wantsCheese){
+                sandwichType = new Cheese(sandwichType);
+            }boolean wantsLettuce = rnd.nextBoolean();
+            if (wantsLettuce){
+                sandwichType = new Lettuce(sandwichType);
+            }boolean wantsTomato = rnd.nextBoolean();
+            if (wantsTomato){
+                sandwichType = new Tomato(sandwichType);
+            }
+            wantsToppings = rnd.nextBoolean();
+        }
+    }
+
+    public void setSandwichType(){
+        int sTypeInt = rnd.nextInt(3);
+        sandwichType = sandwichHashMap.get(sTypeInt+1);
+    }
+
+    public float cost(){
+        costOfsandwich = Float.parseFloat(df.format(sandwichType.cost()));
+        return costOfsandwich;
+    }
+
 }
+
+
 
