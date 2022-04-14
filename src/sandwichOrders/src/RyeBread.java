a0f0c7e1a5ba35c06f75968dba3687060d53e886
Thomas Abato <abatot@moravian.edu>
2022-04-14 06:38:18 -0400

    Rye Bread Java Documentation (Corrections)

--- src/sandwichOrders/src/RyeBread.java
+++ src/sandwichOrders/src/RyeBread.java
@@ -1,16 +1,36 @@
 package sandwichOrders2.src;
 
+/*
+Author : @ Jake Clause
+
+Description - Part of the decorator method, adds Rye Bread
+ */
 public class RyeBread extends CondimentDecorator {
     Sandwich sandwich;
 
+    /*
+Author : @ Jake Clause
+
+Description - Constructor given a sandwich
+ */
     public RyeBread(Sandwich sandwich) {
         this.sandwich = sandwich;
     }
 
+    /*
+Author : @ Jake Clause
+
+Description - Basic getter method, returns description of Rye Bread
+ */
     public String getDescription() {
         return sandwich.getDescription() + ", Rye Bread";
     }
 
+    /*
+Author : @ Jake Clause
+
+Description - Basic return method for the sandiwch cost given Rye Bread is added
+ */
     public double cost() {
         return .30 + sandwich.cost();
     }
