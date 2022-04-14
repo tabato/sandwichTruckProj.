000fa4ef1220e6b689092690642087898b1a7315
Thomas Abato <abatot@moravian.edu>
2022-04-14 06:44:47 -0400

    Condiment Decorator Java Documentation (Corrections)

--- src/sandwichOrders/src/CondimentDecorator.java
+++ src/sandwichOrders/src/CondimentDecorator.java
@@ -1,5 +1,10 @@
 package sandwichOrders2.src;
 
+/*
+Author : @ Jake Clause
+
+Description - Abstract class for the condiments to be instatiated through
+ */
 public abstract class CondimentDecorator extends Sandwich {
     public abstract String getDescription();
 }
