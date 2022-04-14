4e8038e30bbba4eac2d55be679c0c2e23fa83c3f
Thomas Abato <abatot@moravian.edu>
2022-04-14 06:23:01 -0400

    Bun Java Documentation (Corrections)

--- src/sandwichOrders/src/Bun.java
+++ src/sandwichOrders/src/Bun.java
@@ -1,16 +1,37 @@
 package sandwichOrders2.src;
 
+/*
+Author : @ Jake Clause
+
+Description - Part of the decorator method, adds a bun
+ */
+
 public class Bun extends CondimentDecorator {
     Sandwich sandwich;
 
+    /*
+Author : @ Jake Clause
+
+Description - Constructor given a sandwich
+ */
     public Bun(Sandwich sandwich) {
         this.sandwich = sandwich;
     }
 
+    /*
+Author : @ Jake Clause
+
+Description - Basic getter method, returns description of a bun
+ */
     public String getDescription() {
         return sandwich.getDescription() + ", Bun";
     }
 
+    /*
+Author : @ Jake Clause
+
+Description - Basic return method for the sandiwch cost given a bun is added
+ */
     public double cost() {
         return .15 + sandwich.cost();
     }
