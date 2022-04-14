b155582cfa8736d846a7cc18f82e1edf4b2f8af6
Thomas Abato <abatot@moravian.edu>
2022-04-14 06:42:55 -0400

    Wheat Bread Java Documentation (Corrections)

--- src/sandwichOrders/src/WheatBread.java
+++ src/sandwichOrders/src/WheatBread.java
@@ -1,16 +1,36 @@
 package sandwichOrders2.src;
 
+/*
+Author : @ Jake Clause
+
+Description - Part of the decorator method, adds Wheat Bread
+ */
 public class WheatBread extends CondimentDecorator {
     Sandwich sandwich;
 
+    /*
+Author : @ Jake Clause
+
+Description - Constructor given a sandwich
+ */
     public WheatBread(Sandwich sandwich) {
         this.sandwich = sandwich;
     }
 
+    /*
+Author : @ Jake Clause
+
+Description - Basic getter method, returns description of Wheat Bread
+ */
     public String getDescription() {
         return sandwich.getDescription() + ", Wheat Bread";
     }
 
+    /*
+Author : @ Jake Clause
+
+Description - Basic return method for the sandiwch cost given Wheat Bread is added
+ */
     public double cost() {
         return .45 + sandwich.cost();
     }
