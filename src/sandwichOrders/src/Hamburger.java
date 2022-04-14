4ad5b0183bf179da115b3aac809a5dc7f8e5445d
Thomas Abato <abatot@moravian.edu>
2022-04-14 06:29:26 -0400

    Hamburger Sandwich Java Documentation (Corrections)

--- src/sandwichOrders/src/Hamburger.java
+++ src/sandwichOrders/src/Hamburger.java
@@ -1,11 +1,26 @@
 package sandwichOrders2.src;
 
+/*
+Author : @ Jake Clause
+
+Description - A type of sandwich object
+ */
 public class Hamburger extends Sandwich {
 
+    /*
+Author : @ Jake Clause
+
+Description - Constructor given a sandwich
+ */
     public Hamburger() {
         description = "Hamburger";
     }
 
+    /*
+Author : @ Jake Clause
+
+Description - Basic return method for the sandwich cost given a Hamburger sandwich is created
+ */
     public double cost() {
         return 2.00;
     }
