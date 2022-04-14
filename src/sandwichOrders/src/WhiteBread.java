7c8acf3bbafaa40c3264df5289c638c6ba26e1d6
Thomas Abato <abatot@moravian.edu>
2022-04-14 06:43:44 -0400

    White Bread Java Documentation (Corrections)

--- src/sandwichOrders/src/WhiteBread.java
+++ src/sandwichOrders/src/WhiteBread.java
@@ -1,16 +1,36 @@
 package sandwichOrders2.src;
 
+/*
+Author : @ Jake Clause
+
+Description - Part of the decorator method, adds White Bread
+ */
 public class WhiteBread extends CondimentDecorator {
     Sandwich sandwich;
 
+    /*
+Author : @ Jake Clause
+
+Description - Constructor given a sandwich
+ */
     public WhiteBread(Sandwich sandwich) {
         this.sandwich = sandwich;
     }
 
+    /*
+Author : @ Jake Clause
+
+Description - Basic getter method, returns description of White Bread
+ */
     public String getDescription() {
         return sandwich.getDescription() + ", White Bread";
     }
 
+    /*
+Author : @ Jake Clause
+
+Description - Basic return method for the sandiwch cost given White Bread is added
+ */
     public double cost() {
         return .35 + sandwich.cost();
     }
