2d672078970184a03c39da21dffc5c14867c4429
Thomas Abato <abatot@moravian.edu>
2022-04-14 06:32:19 -0400

    Lettuce Java Documentation (Corrections)

--- src/sandwichOrders/src/Lettuce.java
+++ src/sandwichOrders/src/Lettuce.java
@@ -1,16 +1,37 @@
 package sandwichOrders2.src;
 
+package sandwichOrders2.src;
+/*
+Author : @ Jake Clause
+
+Description - Part of the decorator method, adds Lettuce
+ */
 public class Lettuce extends CondimentDecorator {
     Sandwich sandwich;
 
+    /*
+Author : @ Jake Clause
+
+Description - Constructor given a sandwich
+ */
     public Lettuce(Sandwich sandwich) {
         this.sandwich = sandwich;
     }
 
+    /*
+Author : @ Jake Clause
+
+Description - Basic getter method, returns description of Lettuce
+ */
     public String getDescription() {
         return sandwich.getDescription() + ", Lettuce";
     }
 
+    /*
+Author : @ Jake Clause
+
+Description - Basic return method for the sandiwch cost given Lettuce is added
+ */
     public double cost() {
         return .3 + sandwich.cost();
     }
