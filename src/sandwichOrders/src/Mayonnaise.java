922ec5600ef58e87e6657e6e6438df206871e13d
Thomas Abato <abatot@moravian.edu>
2022-04-14 06:33:55 -0400

    Mayonnaise Java Documentation (Corrections)

--- src/sandwichOrders/src/Mayonnaise.java
+++ src/sandwichOrders/src/Mayonnaise.java
@@ -1,16 +1,36 @@
 package sandwichOrders2.src;
 
+/*
+Author : @ Jake Clause
+
+Description - Part of the decorator method, adds Mayonnaise
+ */
 public class Mayonnaise extends CondimentDecorator {
     Sandwich sandwich;
 
+    /*
+Author : @ Jake Clause
+
+Description - Constructor given a sandwich
+ */
     public Mayonnaise(Sandwich sandwich) {
         this.sandwich = sandwich;
     }
 
+    /*
+Author : @ Jake Clause
+
+Description - Basic getter method, returns description of Mayonnaise
+ */
     public String getDescription() {
         return sandwich.getDescription() + ", Mayonnaise";
     }
 
+    /*
+Author : @ Jake Clause
+
+Description - Basic return method for the sandiwch cost given Mayonnaise is added
+ */
     public double cost() {
         return .25 + sandwich.cost();
     }
