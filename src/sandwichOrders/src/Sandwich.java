39760f2815b52601caa39c4190033bd44763df6a
Thomas Abato <abatot@moravian.edu>
2022-04-14 06:45:59 -0400

    Sandwich Java Documentation (Corrections)

--- src/sandwichOrders/src/Sandwich.java
+++ src/sandwichOrders/src/Sandwich.java
@@ -1,11 +1,26 @@
 package sandwichOrders2.src;
 
+/*
+Author : @ Jake Clause
+
+Description - Abstract class for the sandwiches to be made after, works a lot like an interface
+ */
 public abstract class Sandwich {
     String description = "Unknown Sandwich";
 
+    /*
+Author : @ Jake Clause
+
+Description - Abstract class method for all of the sandwiches to have a way of getting their general description
+ */
     public String getDescription() {
         return description;
     }
 
+    /*
+Author : @ Jake Clause
+
+Description - Abstract class method for all of the sandwiches to have a way of getting their total cost for the sandwich
+ */
     public abstract double cost();
 }
