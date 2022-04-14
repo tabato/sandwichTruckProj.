0229fc150b84e1f85a8a1c8a47efa0eaae60c763
Thomas Abato <abatot@moravian.edu>
2022-04-13 20:39:53 -0400

    Direction Java Documentation (Done After The Fact)

--- src/Simulation/Direction.java
+++ src/Simulation/Direction.java
@@ -1,5 +1,12 @@
 package primarySrc.sandwichTruckGUItest.sandwichTruckGUItest.src.com.company.Simulation;
 
+    /*
+    Author: @ Thomas Abato
+
+    Description:
+    - Enum created for directions to be more clear
+     */
+
 public enum Direction
 {
     NORTH, EAST, SOUTH, WEST, NONE
