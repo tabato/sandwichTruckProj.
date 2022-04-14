14aef0ec61d09feaebcd66fa962b547d67011a1e
Thomas Abato <abatot@moravian.edu>
2022-04-14 06:41:49 -0400

    Turkey Sandwich Java Documentation (Corrections)

--- src/sandwichOrders/src/Turkey.java
+++ src/sandwichOrders/src/Turkey.java
@@ -1,11 +1,26 @@
 package sandwichOrders2.src;
 
+/*
+Author : @ Jake Clause
+
+Description - A type of sandwich object
+ */
 public class Turkey extends Sandwich {
 
+    /*
+Author : @ Jake Clause
+
+Description - Constructor given a sandwich
+ */
     public Turkey() {
         description = "Turkey Sandwich";
     }
 
+    /*
+Author : @ Jake Clause
+
+Description - Basic return method for the sandwich cost given a Turkey sandwich is created
+ */
     public double cost() {
         return 1.50;
     }
