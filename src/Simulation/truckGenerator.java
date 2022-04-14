96abdf68b48d1976ea2dac26dff3548094aa441f
Thomas Abato <abatot@moravian.edu>
2022-04-13 22:13:46 -0400

    Truck Generator Java Documentation (Done After The Fact)

--- src/Simulation/truckGenerator.java
+++ src/Simulation/truckGenerator.java
@@ -2,13 +2,32 @@ package primarySrc.sandwichTruckGUItest.sandwichTruckGUItest.src.com.company.Sim
 
 import java.io.IOException;
 
+    /*
+    Author: @ Thomas Abato
+
+    Directions - Interface to construct the truck into the GUI and also to the observers
+    */
 public interface
 truckGenerator {
 
+        /*
+        Author: @ Thomas Abato
+
+        Directions - Registers the truck as an observer
+        */
     void registerObserver(Observer display);
 
+        /*
+        Author: @ Thomas Abato
+
+        Directions - Removes the truck as an observer
+        */
     void removeObserver(Observer display);
 
+        /*
+            Author: @ Thomas Abato
 
+            Directions - Notifys the observers acting as an update method
+            */
     void notifyObserver(Order location) throws IOException;
 }
