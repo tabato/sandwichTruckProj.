e7f98c7cd92ed290492c78660a7a69c90fe6b19a
ColinConway <conwayc@moravian.edu>
2022-04-13 21:04:32 -0400

    JavaDocs

--- src/Simulation/RouteMethod.java
+++ src/Simulation/RouteMethod.java
@@ -3,6 +3,12 @@ package primarySrc.sandwichTruckGUItest.sandwichTruckGUItest.src.com.company.Sim
 import java.awt.Point;
 import java.util.PriorityQueue;
 
+/*
+    interface routeMethod
+    The Route interface is the implementation of a Strategy design pattern within Simulation. The calculateRoute method
+    is passed a PriorityQueue of orders and the distribution center's location.
+    @author Colin Conway
+ */
 public interface RouteMethod
 {
     Route calculateRoute(PriorityQueue<Order> orders, Point distrCenter);
