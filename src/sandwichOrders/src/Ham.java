16292936112fd8d8c37d0d0d750afb4a91173725
Thomas Abato <abatot@moravian.edu>
2022-04-14 06:27:04 -0400

    Ham Sandwich Java Documentation (Corrections)

--- src/sandwichOrders/src/Ham.java
+++ src/sandwichOrders/src/Ham.java
@@ -1,11 +1,26 @@
 package sandwichOrders2.src;
 
+/*
+Author : @ Jake Clause
+
+Description - A type of sandwich object
+ */
 public class Ham extends Sandwich {
 
+    /*
+Author : @ Jake Clause
+
+Description - Constructor given a sandwich
+ */
     public Ham() {
         description = "Ham Sandwich";
     }
 
+    /*
+Author : @ Jake Clause
+
+Description - Basic return method for the sandiwch cost given a ham sandwich is created
+ */
     public double cost() {
         return 1.50;
     }
