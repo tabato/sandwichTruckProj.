74fb8edaa098bd9b631d3a529895408ee336f7d1
Thomas Abato <abatot@moravian.edu>
2022-04-14 06:41:02 -0400

    Tomato Java Documentation (Corrections)

--- src/sandwichOrders/src/Tomato.java
+++ src/sandwichOrders/src/Tomato.java
@@ -1,16 +1,35 @@
 package sandwichOrders2.src;
 
+/*
+Author : @ Jake Clause
+
+Description - Part of the decorator method, adds Tomato
+ */
 public class Tomato extends CondimentDecorator {
     Sandwich sandwich;
 
+    /*
+Author : @ Jake Clause
+Description - Constructor given a sandwich
+ */
     public Tomato(Sandwich sandwich) {
         this.sandwich = sandwich;
     }
 
+    /*
+Author : @ Jake Clause
+
+Description - Basic getter method, returns description of Tomato
+ */
     public String getDescription() {
         return sandwich.getDescription() + ", Tomato";
     }
 
+    /*
+Author : @ Jake Clause
+
+Description - Basic return method for the sandiwch cost given Tomato is added
+ */
     public double cost() {
         return .40 + sandwich.cost();
     }
