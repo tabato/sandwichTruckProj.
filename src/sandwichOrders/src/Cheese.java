1b8fe2a940df69f53290230d86ecf75582ca8bd2
Thomas Abato <abatot@moravian.edu>
2022-04-14 06:25:29 -0400

    Cheese Java Documentation (Corrections)

--- src/sandwichOrders/src/Cheese.java
+++ src/sandwichOrders/src/Cheese.java
@@ -1,16 +1,36 @@
 package sandwichOrders2.src;
 
+/*
+Author : @ Jake Clause
+
+Description - Part of the decorator method, adds cheese
+ */
 public class Cheese extends CondimentDecorator {
     Sandwich sandwich;
 
+    /*
+Author : @ Jake Clause
+
+Description - Constructor given a sandwich
+ */
     public Cheese(Sandwich sandwich) {
         this.sandwich = sandwich;
     }
 
+    /*
+Author : @ Jake Clause
+
+Description - Basic getter method, returns description of cheese
+ */
     public String getDescription() {
         return sandwich.getDescription() + ", Cheese";
     }
 
+    /*
+Author : @ Jake Clause
+
+Description - Basic return method for the sandiwch cost given cheese is added
+ */
     public double cost() {
         return .75 + sandwich.cost();
     }
